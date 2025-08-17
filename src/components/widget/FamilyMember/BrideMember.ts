e521fe644b0c93cb46191caabbd6db840bb32af3
Pravendra jain <pravendrajain421@gmail.com>
2025-08-17 19:18:45 +0530

    add the image

--- src/components/widget/FamilyMember/BrideMember.ts
+++ src/components/widget/FamilyMember/BrideMember.ts
@@ -7,39 +7,39 @@ export interface FamilyMemberData{
 export const BrideMemberItem:FamilyMemberData[]=[
     {
         id:1,
-        imageUrl:"https://res.cloudinary.com/ddguf7pkw/image/upload/v1754549835/Gemini_Generated_Image_d37ezjd37ezjd37e_a5zyiq.png",
-        name:"Name",
-        relation:"Relation"
+        imageUrl:"https://res.cloudinary.com/ddguf7pkw/image/upload/c_fill,w_350,h_350/v1755436400/taupic_yolyp3.jpg",
+        name:"Mr.Ashok Jain",
+        relation:"I would like you to meet my Tauji"
     },
     {
         id:2,
-        imageUrl:"https://res.cloudinary.com/ddguf7pkw/image/upload/v1754549835/Gemini_Generated_Image_d37ezjd37ezjd37e_a5zyiq.png",
-        name:"Name",
-        relation:"Relation"
+        imageUrl:"https://res.cloudinary.com/ddguf7pkw/image/upload/c_fill,w_350,h_350/v1755436765/taiPic_qhywxk.jpg",
+        name:"Mrs.Roopbati Jain",
+        relation:"I would like you to meet my Taiji"
     },
     {
         id:3,
-        imageUrl:"https://res.cloudinary.com/ddguf7pkw/image/upload/v1754549835/Gemini_Generated_Image_d37ezjd37ezjd37e_a5zyiq.png",
-        name:"Name",
-        relation:"Relation"
+        imageUrl:"https://res.cloudinary.com/ddguf7pkw/image/upload/c_fill,w_350,h_350/v1755436858/papajipic_oj1vfj.jpg",
+        name:"Mr. Ajit Jain",
+        relation:"I'd love for you to meet my dad."
     },
     {
         id:4,
-        imageUrl:"https://res.cloudinary.com/ddguf7pkw/image/upload/v1754549835/Gemini_Generated_Image_d37ezjd37ezjd37e_a5zyiq.png",
-        name:"Name",
-        relation:"Relation"
+        imageUrl:"https://res.cloudinary.com/ddguf7pkw/image/upload/c_fill,w_350,h_350/v1755437033/mummy_cjmilz.jpg",
+        name:"Mrs.Mamta jain",
+        relation:"I'd love for you to meet my mom."
     },
     
     {
         id:5,
-        imageUrl:"https://res.cloudinary.com/ddguf7pkw/image/upload/v1754549835/Gemini_Generated_Image_d37ezjd37ezjd37e_a5zyiq.png",
-        name:"Name",
-        relation:"Relation"
+        imageUrl:"https://res.cloudinary.com/ddguf7pkw/image/upload/c_fill,w_350,h_350/v1755437276/bhaiya_dq15ti.jpg",
+        name:"RajKumar jain",
+        relation:"Meet my wonderful brother"
     },
     {
         id:6,
-        imageUrl:"https://res.cloudinary.com/ddguf7pkw/image/upload/v1754549835/Gemini_Generated_Image_d37ezjd37ezjd37e_a5zyiq.png",
-        name:"Name",
-        relation:"Relation"
+        imageUrl:"https://res.cloudinary.com/ddguf7pkw/image/upload/v1755437453/bhabhi_rgdgft.jpg",
+        name:"Julie Jain",
+        relation:"Please meet my Bhabhi"
     }
 ]
